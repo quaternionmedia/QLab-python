bf2e3916756dda7e16424c99e8f8cd078a2f2895
Harpo <harpo@harpo.me>
2024-02-25 14:35:42 -0800

    ⌨️ Adds Pydantic Cue model

--- qlab/cues.py
+++ qlab/cues.py
@@ -1,6 +1,19 @@
 from csv import DictReader
+from uuid import UUID
+
+from pydantic import BaseModel
 
 from qlab import QLab
+
+
+class Cue(BaseModel):
+    uniqueID: UUID
+
+    number: str
+    name: str
+    type: str
+    notes: str | None = None
+
 
 NAMES = {
     'Lights': '',
@@ -26,12 +39,23 @@ def parse_cuelist(cuelist):
 
 class Cues:
     def __init__(self, csv):
-        self.cues = open_csv(csv)
+        self.csv_cues = open_csv(csv)
         self.q = QLab()
         self.cuelists = self.q.send('/cueLists')['data']
-        self.cuelist = parse_cuelist(self.cuelists[0])
+        self.cues = parse_cuelist(self.cuelists[0])
 
     def sync_cuelist(self):
         """Synchronize the cuelist with the cues in the csv"""
-        for cue in self.cues:
-            existing_cue = self.q.get_cue_property(cue['number'], 'number')
+        for cue in self.csv_cues:
+            if cue['number'] in self.cues:
+                self.update_cue(cue)
+            else:
+                self.create_cue(cue)
+
+    def update_cue(self, cue):
+        """Update a cue"""
+        pass
+
+    def create_cue(self, cue):
+        """Create a cue"""
+        pass
